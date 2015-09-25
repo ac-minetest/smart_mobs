bc9621e64272c1c34eda4f280e34944435b1e164
TenPlus1 <kinsellaja@yahoo.com>
2015-09-25 09:20:01 +0100

    Fixed missing entity dist check

--- api.lua
+++ api.lua
@@ -1,4 +1,4 @@
--- Mobs Api (24th September 2015)
+-- Mobs Api (25th September 2015)
 mobs = {}
 mobs.mod = "redo"
 
@@ -768,11 +768,13 @@ end
 		-- calculate distance from mob and enemy
 		local s = self.object:getpos()
 		local p = self.attack.player:getpos()
-		if not p then p = s end
-		local dist = ((p.x - s.x) ^ 2 + (p.y - s.y) ^ 2 + (p.z - s.z) ^ 2) ^ 0.5
+		if p then
+			local dist = ((p.x - s.x) ^ 2 + (p.y - s.y) ^ 2 + (p.z - s.z) ^ 2) ^ 0.5
+		end
 
 		-- stop attacking if no player or out of range
-		if dist > self.view_range
+		if not p
+		or dist > self.view_range
 		or not self.attack.player
 		or not self.attack.player:getpos()
 		or self.attack.player:get_hp() <= 0 then
